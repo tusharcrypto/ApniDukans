0b373f4cc819c1cbac5a481207ae22ea382b8a80
vinayak <vinayakutture003@gmail.com>
2024-04-18 17:48:28 +0530

    "Handled JWT-Token"

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -1,24 +1,75 @@
 
-import React, { useState } from 'react'
-import './CSS/loginSignup.css'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import './CSS/loginSignup.css';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { jwtDecode } from 'jwt-decode';
 
 export default function Login() {
-  const logindetail = {email:"",password:""};
+  const logindetail = {UserName:"",Password:""};
   const [loigndata,setlogindata] = useState(logindetail);
+  const [token,settoken] = useState("");
+
+ useEffect(()=>{
+
+ const token= localStorage.getItem('token');
+
+ if (token) {
+  // Decode the token
+  const decodedToken = jwtDecode(token);
+  // Access the role from the decoded token
+  const userRole = decodedToken.role;
+
+  // Redirect the user based on their role
+  if (userRole === 'admin') {
+    // Redirect to the admin page
+    window.location.href = '/home';
+  } else if (userRole === 'student') {
+    // Redirect to the user page
+    window.location.href = '/men';
+  } else {
+    // Handle other roles or unauthorized access
+    console.error('Unknown role or unauthorized access');
+  }
+} else {
+  console.error('Token not found');
+}
+
+ },[])
+
   const handlelogin =(e)=>{
     setlogindata({...loigndata,[e.target.name]:e.target.value})
   }
   const handlesubmit =(e)=>{
     e.preventDefault();
     // axios.post('http://localhost:5000/signup',userdata).then((res)=>{
-    axios.post("http://localhost:5000/login",loigndata).then((res)=>{
+    axios.post("https://localhost:7215/api/Auth/login",loigndata).then((res)=>{
       console.log(res.data)
-      if(res.data==true) window.location.href="/"  
-      else{
-        window.location.href='/men'
-      }  
+      const token= res.data
+      localStorage.setItem('token',token)
+      settoken(token)
+
+      // Decode the token
+  const decodedToken = jwtDecode(token);
+  // Access the role from the decoded token
+  const userRole = decodedToken.role;
+
+  // Redirect the user based on their role
+  if (userRole === 'admin') {
+    // Redirect to the admin page
+    window.location.href = '/home';
+  } else if (userRole === 'student') {
+    // Redirect to the user page
+    window.location.href = '/men';
+  } else {
+    // Handle other roles or unauthorized access
+    console.error('Unknown role or unauthorized access');
+  }
+
+      //if(res.data==true) window.location.href="/"  
+      // else{
+      //   window.location.href='/men'
+      // }  
     })
 
   }
@@ -28,8 +79,9 @@ export default function Login() {
         <h1>Login</h1>
         <div className="loginSignup-fields"> 
           {/* <input type='text' placeholder='Enter the Name'></input> */}
-          <input type='email' name='email' value={loigndata.email} onChange={handlelogin} placeholder='Enter the Email'></input>
-          <input type='password' name='password' value={loigndata.password} onChange={handlelogin} placeholder='Enter the password'></input>
+          {/* <input type='email' name='email' value={loigndata.email} onChange={handlelogin} placeholder='Enter the Email'></input> */}
+          <input type='text'  name='UserName' value={loigndata.UserName} placeholder='Enter the Name' onChange={handlelogin}></input>
+          <input type='password' name='Password' value={loigndata.Password} onChange={handlelogin} placeholder='Enter the password'></input>
         </div>
         <button onClick={handlesubmit}>Continue</button>
         <p className='loginsignup-login'>Not Register Yet ? <span> <Link to='/signup'>Sign Up</Link> </span></p>
